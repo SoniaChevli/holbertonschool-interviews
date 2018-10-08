ec7f9381042f454753d1014c279a09d190ee27e3
Sonia Chevli <soniachevli@gmail.com>
2018-10-08 16:11:51 +0000

    first commit

---
+++ 0x01-warm_up/0-binary.c
@@ -0,0 +1,65 @@
+#include "search_algos.h"
+/**
+ * print_array - prints the array
+ * @array: array to print
+ * @min: min number
+ * @max: max number
+ *
+ * Return: nothing
+ */
+void print_array(int *array, int min, int max)
+{
+	printf("Searching in array: ");
+	while (min < max)
+	{
+		if (min == max - 1)
+			printf("%d", array[min]);
+		else
+			printf("%d, ", array[min]);
+		min++;
+	}
+	printf("\n");
+
+}
+/**
+ * binary_search - searches for a value in a sorted array
+ * @array: array to search
+ * @size: size of array searching
+ * @value: value to search for
+ *
+ * Return: index where value is located. Otherwise -1.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int min;
+	unsigned int mid;
+	unsigned int max;
+
+	if (array == NULL)
+		return (-1);
+
+	mid = size / 2;
+	max = size;
+	min = 0;
+	print_array(array, min, max);
+	while (min != size)
+	{
+		if (value == array[mid])
+			return (mid);
+		else if (min == max - 1)
+			return (-1);
+		else if (value > array[mid])
+		{
+			min = mid;
+			mid = ((max - min) / 2) + min;
+		}
+		else
+		{
+			max = mid;
+			mid = max / 2;
+		}
+		print_array(array, min, max);
+	}
+	return (-1);
+
+}
---
+++ 0x01-warm_up/search_algos.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include <stdlib.h>
+int binary_search(int *array, size_t size, int value);
